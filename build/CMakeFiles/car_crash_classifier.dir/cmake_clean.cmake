6e2421513975d4264baa1311a05083ddbf4e217a
Filipy <filipydasilvacorporativo@gmail.com>
2026-06-05 18:15:17 -0300

    Created Window

---
+++ build/CMakeFiles/car_crash_classifier.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_crash_classifier.dir/link.d"
+  "CMakeFiles/car_crash_classifier.dir/src/main.cpp.o"
+  "CMakeFiles/car_crash_classifier.dir/src/main.cpp.o.d"
+  "car_crash_classifier"
+  "car_crash_classifier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/car_crash_classifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/car_crash_classifier.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for car_crash_classifier.
+# This may be replaced when dependencies are built.
